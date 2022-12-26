6e6e8aeb03f21c0642e4f37acd4fdc05d561d12f
ErdbeerbaerLP <admin@erdbeerbaerlp.de>
2022-12-27 00:54:23 +0100

    Keep NowPlaying active if queue is empty

--- src/main/java/com/jagrosh/jmusicbot/audio/NowplayingHandler.java
+++ src/main/java/com/jagrosh/jmusicbot/audio/NowplayingHandler.java
@@ -85,7 +85,6 @@ public class NowplayingHandler
             if(msg==null)
             {
                 msg = handler.getNoMusicPlaying(bot.getJDA());
-                toRemove.add(guildId);
             }
             try 
             {
