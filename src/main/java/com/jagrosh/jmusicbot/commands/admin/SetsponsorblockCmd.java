871df0924882504753c03b0cdd247ae56879485b
ErdbeerbaerLP <admin@erdbeerbaerlp.de>
2024-11-27 14:01:59 +0100

    Optimize SetsponsorblockCmd autocomplete fix wrong option being used

--- src/main/java/com/jagrosh/jmusicbot/commands/admin/SetsponsorblockCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/admin/SetsponsorblockCmd.java
@@ -48,17 +48,26 @@ public class SetsponsorblockCmd extends AdminCommand {
     @Override
     public void onAutoComplete(CommandAutoCompleteInteractionEvent event) {
         final ArrayList<net.dv8tion.jda.api.interactions.commands.Command.Choice> choices = new ArrayList<>();
-        final String[] split = event.getFocusedOption().getValue().split(",");
+        final String input = event.getFocusedOption().getValue();
+        final String[] split = input.split(",");
         if(split.length == 1) choices.add(new Command.Choice("off", "off"));
-
-        String prefix = split.length>1?String.join(",", Arrays.copyOfRange(split,0,split.length-1)):"";
+        String prefix;
+        if (input.endsWith(",")) {
+            // If the input ends with a comma, return it as is.
+            prefix = input;
+        } else if (input.contains(",")) {
+            // If the input does not end with a comma but contains commas,
+            // find the last complete segment before the last comma.
+            int lastCommaIndex = input.lastIndexOf(",");
+            prefix = input.substring(0, lastCommaIndex + 1); // Include up to the last comma.
+        } else {
+            // If the input has no commas, return an empty string.
+            prefix = "";
+        }
         final String val = event.getFocusedOption().getValue().endsWith(",")?"":split[split.length-1];
         int count = 0;
         for (de.erdbeerbaerlp.jsponsorblock.Category cat : de.erdbeerbaerlp.jsponsorblock.Category.values()) {
             String name = cat.name();
-            for(final String s : split){
-                if(s.equalsIgnoreCase(name)) continue;
-            }
             if (cat.name().toLowerCase().contains(val.toLowerCase())) {
                 choices.add(new net.dv8tion.jda.api.interactions.commands.Command.Choice(prefix+name, prefix+name));
                 count++;
@@ -71,17 +80,17 @@ public class SetsponsorblockCmd extends AdminCommand {
 
     @Override
     protected void execute(SlashCommandEvent event) {
-        if (!event.hasOption("category")) {
+        if (!event.hasOption("categories")) {
             event.reply(event.getClient().getError() + " Please include a category or off\nAvailable categories are: `" + Arrays.toString(de.erdbeerbaerlp.jsponsorblock.Category.values()) + "`").setEphemeral(true).queue();
             return;
         }
         Settings s = event.getClient().getSettingsFor(event.getGuild());
-        if (event.getOption("category").getAsString().equalsIgnoreCase("off")) {
+        if (event.getOption("categories").getAsString().equalsIgnoreCase("off")) {
             s.setCategories(new de.erdbeerbaerlp.jsponsorblock.Category[0]);
             event.reply(event.getClient().getSuccess() + " Sponsorblock has now been disabled!").setEphemeral(true).queue();
         } else {
             final ArrayList<de.erdbeerbaerlp.jsponsorblock.Category> cats = new ArrayList<>();
-            for (String str : event.getOption("category").getAsString().replace(" ", "").toUpperCase().split(",")) {
+            for (String str : event.getOption("categories").getAsString().replace(" ", "").toUpperCase().split(",")) {
                 try {
                     cats.add(de.erdbeerbaerlp.jsponsorblock.Category.valueOf(str));
                 } catch (IllegalArgumentException e) {
